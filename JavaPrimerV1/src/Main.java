962f12bb85e7f47353433156ca23a56b273ca38c
ivan95603 <ivan95.603@hotmail.com>
2017-11-10 23:27:39 +0100

    Dodat primer

---
+++ JavaPrimerV1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        int a = 4;
+        int b = 21;
+        int c = a + b;
+        double d =  (double) b / c;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
