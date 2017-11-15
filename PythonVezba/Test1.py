6cc804b35af38c8543685d8b8a894b6fa7569bf4
ivan95603 <ivan95.603@hotmail.com>
2017-11-15 10:49:06 +0100

    Dodat kod sa prvog Python dana

--- PythonVezba/Test1.py
+++ PythonVezba/Test1.py
@@ -14,6 +14,67 @@ tekst = ""
 
 fl = float(455.2)
 
-
-
 print(tekst)
+
+'''print("Hello world!")
+print("Hello world!")
+print("Hello world!")
+print("Hello world!")'''
+
+'''
+for a in range(1, 10):
+    for b in range(1, 10):
+        print("Hello world!", a, b, sep="*-*", end="\n")
+        print("Hello world!")'''
+
+
+'''msg = "Hello world!"
+print(msg)
+msg = 54
+print(msg)
+
+//Liste
+
+bikes = ['trek', 'redline', 'giant', 54, 5.5]
+first_bike = bikes[0]
+for bike in bikes:
+ print(bike)
+'''
+
+
+'''squares = []
+for x in range(1, 11):
+ squares.append(x**2)
+ '''
+
+'''Tuples'''
+'''
+dimensions = (1920, 1080)
+
+for a in dimensions:
+    print(a)
+'''
+
+'''Dictionaries'''
+'''
+alien = {'color': 'green', 'points': 5}
+print("The alien's color is " + alien['color'] + str(alien['points']))
+'''
+
+
+'''User input'''
+'''
+name = input("What's your name? ")
+print("Hello, " + name + "!")
+'''
+
+'''Funkcije'''
+'''
+def greet_user(username):
+    print("Hello, " + username + "!")
+
+greet_user('jesse')
+
+'''
+
+msg = 54
